733c5a2be65df1789f1aac6839fb4239505f4dae
Vlad-Alexandru Dindeal <vlladin@gmail.com>
2025-04-09 13:27:56 +0300

    Base tests

--- tests/core/pyspec/eth2spec/test/eipxxxx_eods/block_processing/test_process_eipxxxx_eods_deposit_to_delegate_request.py
+++ tests/core/pyspec/eth2spec/test/eipxxxx_eods/block_processing/test_process_eipxxxx_eods_deposit_to_delegate_request.py
@@ -1,26 +1,37 @@
 from eth2spec.test.context import spec_state_test, expect_assertion_error, \
     with_eipxxxx_eods_and_later
 
-def empty_block(spec):
-    return spec.BeaconBlock()
-
-def run_process_whisk_opening_proof(spec, state, block, valid=True):
-    yield 'pre', state
-    yield 'block', block
-
-    if not valid:
-        expect_assertion_error(lambda: spec.process_whisk_opening_proof(state, block))
-        yield 'post', None
-        return
-
-    spec.process_whisk_opening_proof(state, block)
-
-    yield 'post', state
-
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_has_valid_signature(spec, state):
+    assert 1 > 1
 
 @with_eipxxxx_eods_and_later
 @spec_state_test
-def test_deposit_to_delegate_no_delegator_available(spec, state):
-    print(spec)
-    print(state)
+def test_deposit_to_delegate_request_creates_delegator_if_needed(spec, state):
+    assert 1 > 1
+
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_does_top_up_if_needed(spec, state):
+    assert 1 > 1
+
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_uses_minimum_deposit_amount(spec, state):
+    assert 1 > 1
+
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_uses_minimum_deposit_amount(spec, state):
+    assert 1 > 1
+
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_increases_topup_delegator_balance(spec, state):
+    assert 1 > 1
+
+@with_eipxxxx_eods_and_later
+@spec_state_test
+def test_deposit_to_delegate_request_increases_new_delegator_balance(spec, state):
     assert 1 > 1
