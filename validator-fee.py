decc7c76ad94015a7fdb51a33bdc765f495000e7
Vlad-Alexandru Dindeal <vlladin@gmail.com>
2025-07-30 01:19:46 +0300

    Added a test script

---
+++ validator-fee.py
@@ -0,0 +1,155 @@
+from typing import List
+
+class DelegatedValidator:
+    delegators_balances: List[float] # this mocks state.delegators[index]
+    validator_balance: float # this mocks state.balances[index]
+
+    delegated_validator_quota: float
+    delegators_quotas: List[float]
+    delegated_balances: List[float]
+    dry_delegated_balances: List[float] # [dev] the amounts delegated over time, without any Rewards/Penalties
+    rewards_penalties_deltas: List[float]
+    total_delegated_balance: float
+    fee_quotient: float
+
+    def __init__(self):
+        self.delegators_balances = []
+        self.validator_balance = 0
+
+        self.delegated_validator_quota = 0
+        self.delegators_quotas = [0,0,0]
+        self.delegated_balances = [0,0,0]
+
+        self.dry_delegated_balances = [0, 0, 0]
+
+        self.rewards_penalties_deltas = [0,0,0]
+        self.total_delegated_balance = 0
+        self.fee_quotient = 0.1
+
+        self.setup_initial_values()
+
+    def setup_initial_values(self)-> None:
+        self.delegators_balances = [100, 100, 100]
+        self.validator_balance = 50
+        self.delegated_validator_quota = 1
+
+
+    def recalculate_delegator_quotas(self) -> None:
+        if self.total_delegated_balance == 0:
+            self.delegated_validator_quota = 1
+        else:
+            self.delegated_validator_quota = (self.validator_balance / (self.total_delegated_balance + self.validator_balance))
+            for index in range(len(self.delegators_quotas)):
+                self.delegators_quotas[index] = self.delegated_balances[index] / self.total_delegated_balance * (1 - self.delegated_validator_quota)
+
+    def delegate_to_validator(self, delegator_index: int, delegated_amount: float) -> None:
+        # here we decrement the delegators available balance
+        self.delegators_balances[delegator_index] -= delegated_amount
+        self.dry_delegated_balances[delegator_index] += delegated_amount
+
+        num_delegated_balances = len(self.delegated_balances)
+
+        # ensure we have enough indexes inside the delegated validator to keep them parallel with delegator_index
+        if (delegator_index > num_delegated_balances):
+            for _ in range(delegator_index - num_delegated_balances):
+                self.delegated_balances.append(0)
+                self.delegated_quotas.append(0)
+
+        # here we increase the delegated balance from this specific delegator, under this delegated validator, with delegated amount
+        self.delegated_balances[delegator_index] += delegated_amount
+
+        # here we increase the delegated validator's total delegated balance with delegated amount
+        self.total_delegated_balance += delegated_amount
+
+        # here we recalculate delegators' quotas under this delegated validator
+        self.recalculate_delegator_quotas()
+
+    def reward_delegated_validator(self, reward: float) -> None:
+        validator_reward = self.delegated_validator_quota * reward
+        delegators_reward = reward - validator_reward
+
+        # reward the operator
+        self.validator_balance+=validator_reward
+
+        # reward the delegations
+        self.apply_delegations_rewards(delegators_reward)
+
+    def penalize_delegated_validator(self, penalty: float) -> None:
+        validator_penalty = self.delegated_validator_quota * penalty
+        delegators_penalty = penalty - validator_penalty
+
+        # penalize the operator
+        self.validator_balance-=validator_penalty
+
+        # penalize the delegations
+        self.apply_delegations_penalties(delegators_penalty)
+
+    def apply_delegations_rewards(self, amount: float) -> None:
+        self.total_delegated_balance += amount
+
+        for index in range(len(self.delegators_quotas)):
+            self.delegated_balances[index] += amount * self.delegators_quotas[index]
+            self.rewards_penalties_deltas[index] += amount * self.delegators_quotas[index] # this was added for the patch
+
+    def apply_delegations_penalties(self, amount: float) -> None:
+        self.total_delegated_balance -= amount
+
+        for index in range(len(self.delegators_quotas)):
+            self.delegated_balances[index] -= amount * self.delegators_quotas[index]
+            self.rewards_penalties_deltas[index] -= amount * self.delegators_quotas[index] # this was added for the patch
+
+    def slash_pre(self):
+        pass
+
+    def slash_post(self):
+        pass
+
+    def withdraw_pre(self, delegator_index: float, amount: float):
+        pass
+
+    def withdraw_post(self, delegator_index: float, amount: float):
+        pass
+
+class Simulator:
+    dv: DelegatedValidator
+    action_queue: List[List]
+
+    def __init__(self, dv: DelegatedValidator):
+        self.action_queue = []
+        self.dv = dv
+
+    def queue_action(self, action):
+        self.action_queue.append(action)
+
+    def simulate(self):
+        print(chr(27) + "[2J")
+
+        print("=====", "INITIAL", "=====")
+        attrs = vars(self.dv)
+        print('\n'.join("%s: %s" % item for item in attrs.items()))
+        print('\n')
+
+        for action in self.action_queue:
+            print("=====", action[0], "=====")
+            match len(action):
+                case 2:
+                    action[1]()
+                case 3:
+                    action[1](action[2])
+                case 4:
+                    action[1](action[2], action[3])
+
+            attrs = vars(self.dv)
+            print('\n'.join("%s: %s" % item for item in attrs.items()))
+            print('\n')
+
+
+dv = DelegatedValidator()
+simulator = Simulator(dv)
+
+simulator.queue_action(["Delegator 0 delegates 12", dv.delegate_to_validator, 0, 12])
+simulator.queue_action(["Apply 5 reward", dv.reward_delegated_validator, 5])
+simulator.queue_action(["Apply 3 reward", dv.reward_delegated_validator, 3])
+simulator.queue_action(["Apply 4 reward", dv.reward_delegated_validator, 4])
+simulator.queue_action(["Apply 4 penalty", dv.penalize_delegated_validator, 4])
+simulator.simulate()
